9e43f440bb419688f0280dce41540de25d1c988e
chiko256 <raum.web.creator@gmail.com>
2026-04-16 08:53:17 +0900

    高校ワイヤーフレーム

--- old/script.js
+++
@@ -1,9 +0,0 @@
-function scrollToSection(sectionId) {
-    const element = document.getElementById(sectionId);
-    if (element) {
-        element.scrollIntoView({ 
-            behavior: 'smooth',
-            block: 'start'
-        });
-    }
-}
